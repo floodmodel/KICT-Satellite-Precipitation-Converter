f815dafbebb6ce1ccd288408637a20c142dea312
juhye0102 <juhye0102@naver.com>
2024-05-29 15:07:51 +0900

    init

--- Kict_Satellite_Precipitation_Converter_v0.2.0/Kict_Satellite_Precipitation_Converter/test_url.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-res = urllib.request.Request('ftp://rainmap:Niskur+1404@hokusai.eorc.jaxa.jp/standard/v8/hourly/2014/03/01')
-try: 
-    with urllib.request.urlopen(res) as response:
-        data = response.read()
-        print(data.decode('utf-8'))
-except urllib.error.URLError as e:
-    print(e.reason)
